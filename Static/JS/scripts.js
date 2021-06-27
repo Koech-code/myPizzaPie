e3026caabdec306872db23a1074f08b3f924bae2
Koech-code <nixon.koech@moringaschool.com>
2021-06-27 14:21:40 +0300

    deleted two external html files and modified my styles and scripts

---
+++ Static/JS/scripts.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+   alert( "ready!" );
+});
+
+
+
+
+
+// function clickCell() {
+//     var row = document.getElementById("sp1").rows;
+//  for (var i = 0; i < row.length; i++) {
+//         for (var j = 0; j < row[i].cells.length; j++ ) {
+//                     console.log(row[i].cells[j].innerHTML);
+//             row[i].cells[j].addEventListener('click', function(){
+//                     console.log('click');
+//             })
+//         }
+//     }
+// }
+
+
+
+// function totalPrices(){
+//    var smallPizza1=parseInt(prompt("Input your first price: "));
+//    var smallPizza2=parseInt(prompt("Input your second price: "));
+//    alert(smallPizza1+smallPizza2);
+// };
+// totalPrices();
