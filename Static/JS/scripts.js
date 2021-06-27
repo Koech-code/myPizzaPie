dd4e7f80f07c050299c7028569fa6851d47668b1
Koech-code <nixon.koech@moringaschool.com>
2021-06-27 18:57:41 +0300

    added the logic part for pricing

--- Static/JS/scripts.js
+++ Static/JS/scripts.js
@@ -1,28 +1,7 @@
-$( document ).ready(function() {
-   alert( "ready!" );
-});
-
-
-
-
-
-// function clickCell() {
-//     var row = document.getElementById("sp1").rows;
-//  for (var i = 0; i < row.length; i++) {
-//         for (var j = 0; j < row[i].cells.length; j++ ) {
-//                     console.log(row[i].cells[j].innerHTML);
-//             row[i].cells[j].addEventListener('click', function(){
-//                     console.log('click');
-//             })
-//         }
-//     }
-// }
-
-
-
-// function totalPrices(){
-//    var smallPizza1=parseInt(prompt("Input your first price: "));
-//    var smallPizza2=parseInt(prompt("Input your second price: "));
-//    alert(smallPizza1+smallPizza2);
-// };
-// totalPrices();
+function add(){
+  var a,b,c;
+  a=parseFloat(document.getElementById("first").value);
+  b=parseFloat(document.getElementById("second").value);
+  c= a + b;
+  document.getElementById("answer").value= a + b;
+}
